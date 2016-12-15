2ff8466dca1d0d17e569469cb4441e6e18ed27e9
Nikita Akimov <Korchy@users.noreply.github.com>
2016-12-15 09:34:30 +0300

    Create PartRenderPanel.py

---
+++ PartRenderPanel.py
@@ -0,0 +1,62 @@
+import bpy
+
+class PartitionRenderPanel(bpy.types.Panel):
+    bl_idname = 'panel.partitionRenderPanel'
+    bl_label = 'Partition Render'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'render'
+
+    def draw(self, context):
+        self.layout.operator('render.partition_render', icon = 'RENDER_REGION', text = 'Start/Continue Render')
+        self.layout.prop(bpy.context.scene.partition_render_vars, 'xCut')
+        self.layout.prop(bpy.context.scene.partition_render_vars, 'yCut')
+        # self.layout.prop(bpy.data.scenes[0].partition_render_vars, 'partitions')
+
+
+class PartitionRenderVariables(bpy.types.PropertyGroup):
+    xCut = bpy.props.IntProperty(
+        name = 'Blocks By X',
+        description = 'X Axis Number Of Blocks',
+        subtype='UNSIGNED',
+        default = 1,
+        min = 1
+    )
+    yCut = bpy.props.IntProperty(
+        name = 'Blocks By Y',
+        description = 'Y Axis Number Of Blocks',
+        subtype='UNSIGNED',
+        default = 1,
+        min = 1
+    )
+
+class PartitionRenderStatic(bpy.types.PropertyGroup):
+    tmpFileFormat = bpy.props.StringProperty(
+        name = 'Tmp File Format',
+        description = 'Temporary file format to store partitions',
+        default = 'OPEN_EXR'
+    )
+    tmpColorMode = bpy.props.StringProperty(
+        name = 'Tmp Color Mode',
+        description = 'Temporary color mode to store partitions',
+        default = 'RGBA'
+    )
+    tmpFileExtension = bpy.props.StringProperty(
+        name = 'Tmp File Extension',
+        description = 'Temporary file extension to store partitions',
+        default = 'exr'
+    )
+
+
+def register():
+    bpy.utils.register_class(PartitionRenderPanel)
+    bpy.utils.register_class(PartitionRenderVariables)
+    bpy.types.Scene.partition_render_vars = bpy.props.PointerProperty(type = PartitionRenderVariables)
+    bpy.utils.register_class(PartitionRenderStatic)
+    bpy.types.Scene.partition_render_static = bpy.props.PointerProperty(type = PartitionRenderStatic)
+
+def unregister():
+    del bpy.types.Scene.partition_render_vars
+    bpy.utils.unregister_class(PartitionRenderStatic)
+    bpy.utils.unregister_class(PartitionRenderVariables)
+    bpy.utils.unregister_class(PartitionRenderPanel)
