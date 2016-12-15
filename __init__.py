8763c4680da1c97532d30ff038b33fc9d1cfa668
Nikita Akimov <Korchy@users.noreply.github.com>
2016-12-15 09:24:41 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,44 @@
+bl_info = {
+    'name': 'Partition Render',
+    'category': 'Render',
+    'author': 'Nikita Akimov',
+    'version': (0, 0, 1),
+    'blender': (2, 78, 0),
+    'location': 'Properties window > Render tab > Partition Render',
+    'wiki_url': 'http://b3d.interplanety.ru/add-on-partition-render/',
+    'tracker_url': 'http://b3d.interplanety.ru/add-on-partition-render/',
+    'description': 'Allows to split the render into partitions and render them separately'
+}
+
+import sys
+import importlib
+
+modulesNames = ['RenderBorder', 'PartRender', 'PartRenderPanel']
+
+modulesFullNames = []
+for currentModuleName in modulesNames:
+    if 'DEBUG_MODE' in sys.argv:
+        modulesFullNames.append('{}'.format(currentModuleName))
+    else:
+        modulesFullNames.append('{}.{}'.format(__name__, currentModuleName))
+
+for currentModuleName in modulesFullNames:
+    if currentModuleName in sys.modules:
+        importlib.reload(sys.modules[currentModuleName])
+    else:
+        globals()[currentModuleName] = importlib.import_module(currentModuleName)
+
+def register():
+    for currentModuleName in modulesFullNames:
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
+
+def unregister():
+    for currentModuleName in modulesFullNames:
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+
+if __name__ == "__main__":
+    register()
