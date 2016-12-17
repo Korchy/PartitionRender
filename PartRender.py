0ef38fa4b16d83a8409dc4435229d74d502c2c25
Nikita Akimov <Korchy@users.noreply.github.com>
2016-12-17 10:43:00 +0300

    Update PartRender.py

--- PartRender.py
+++ PartRender.py
@@ -14,22 +14,15 @@ class PartRender(bpy.types.Operator):
     timer = None
     windowToRedrawCompositing = bpy.context.window_manager.windows[0].screen.areas[0]
     windowToRedrawCompositingType = 'INFO'
+    oldFileFormat = bpy.context.scene.render.image_settings.file_format
+    oldColorMode = bpy.context.scene.render.image_settings.color_mode
 
     def execute(self, context):
         # прочитать текущую партицию из файла
         # если файла нет - первая и создать
-        self.__class__.dirPath = os.path.dirname(bpy.data.filepath)+os.path.sep+os.path.splitext(os.path.basename(bpy.data.filepath))[0]
-        if not os.path.exists(self.__class__.dirPath):
-            os.makedirs(self.__class__.dirPath)
-        infoFileName = os.path.splitext(os.path.basename(bpy.data.filepath))[0]+'_pr.json'
-        self.__class__.infoFile = self.__class__.dirPath + os.path.sep + infoFileName
-        if os.path.exists(self.__class__.infoFile):
-            with open(self.__class__.infoFile) as partFile:
-                jfile = json.load(partFile)
-                self.__class__.currentPartitionNumber = jfile['currentPartition']
-        else:
-            setNextPartition()
-        if self.__class__.currentPartitionNumber <= bpy.context.scene.partition_render_vars.xCut * bpy.context.scene.partition_render_vars.yCut:
+        self.getInfoFileName()
+        self.getPartitionNumberToRender()
+        if not self.checkFinish():
             # запуск текущей партиции на рендер
             return self.startRenderPartiton(self.__class__.currentPartitionNumber)
         else:
@@ -46,6 +39,26 @@ class PartRender(bpy.types.Operator):
         self.createCompositingNodes()
         context.window_manager.event_timer_remove(self.timer)
         return {'FINISHED'}
+
+    def getPartitionNumberToRender(self):
+        # получение номера партиции для рендера
+        if self.__class__.currentPartitionNumber == 0:
+            if os.path.exists(self.__class__.infoFile):
+                with open(self.__class__.infoFile) as currentFile:
+                    jsonData = json.load(currentFile)
+                    self.__class__.currentPartitionNumber = jsonData['currentPartition']
+                    if bpy.context.scene.partition_render_vars.useRange:
+                        if self.__class__.currentPartitionNumber > bpy.context.scene.partition_render_vars.rangeTo \
+                                or self.__class__.currentPartitionNumber < bpy.context.scene.partition_render_vars.rangeFrom:
+                            self.__class__.currentPartitionNumber = bpy.context.scene.partition_render_vars.rangeFrom - 1
+                            setNextPartition()
+            else:
+                setNextPartition()
+        # проверить по диапазону
+        if bpy.context.scene.partition_render_vars.useRange:
+            if self.__class__.currentPartitionNumber < bpy.context.scene.partition_render_vars.rangeFrom:
+                self.__class__.currentPartitionNumber = bpy.context.scene.partition_render_vars.rangeFrom - 1
+                setNextPartition()
 
     def startRenderPartiton(self, partitionNumber):
         # Ренднер партиции partitionNumber
@@ -80,10 +93,13 @@ class PartRender(bpy.types.Operator):
         else:
             return {'FINISHED'}
 
-    def resetPartitions(self):
-        self.__class__.currentPartitionNumber = 0
-        if self.__class__.infoFile and os.path.exists(self.__class__.infoFile):
-            os.remove(self.__class__.infoFile)
+    @classmethod
+    def resetPartitions(cls):
+        cls.currentPartitionNumber = 0
+        if cls.infoFile and os.path.exists(cls.infoFile):
+            os.remove(cls.infoFile)
+        if bpy.context.scene.render.use_border:
+            bpy.context.scene.render.use_border = False
         return {'FINISHED'}
 
     def createCompositingNodes(self):
@@ -107,9 +123,17 @@ class PartRender(bpy.types.Operator):
         groupOutput = group.nodes.new(type = 'NodeGroupOutput')
         groupOutput.location = (500, 100)
         # открыть все сохраненные партиции, создать под них ноды и связать их
+        partitionsStart = 0
+        partitionsEnd = bpy.context.scene.partition_render_vars.xCut * bpy.context.scene.partition_render_vars.yCut
         partitionsCount = bpy.context.scene.partition_render_vars.xCut * bpy.context.scene.partition_render_vars.yCut
-        for i in range(partitionsCount):
-            bpy.ops.image.open(filepath = self.__class__.dirPath + os.path.sep + 'p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension, directory = self.__class__.dirPath + os.path.sep)
+        if bpy.context.scene.partition_render_vars.useRange:
+            partitionsStart = bpy.context.scene.partition_render_vars.rangeFrom - 1
+            partitionsEnd = bpy.context.scene.partition_render_vars.rangeTo
+            partitionsCount = bpy.context.scene.partition_render_vars.rangeTo - bpy.context.scene.partition_render_vars.rangeFrom + 1
+        for i in range(partitionsStart, partitionsEnd):
+            imageName = 'p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
+            if os.path.exists(self.__class__.dirPath + os.path.sep + imageName):
+                bpy.ops.image.open(filepath = self.__class__.dirPath + os.path.sep + imageName, directory = self.__class__.dirPath + os.path.sep)
             # Image
             currentImageNode = group.nodes.new(type = 'CompositorNodeImage')
             currentImageNode.image = bpy.data.images['p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension]
@@ -118,23 +142,24 @@ class PartRender(bpy.types.Operator):
             currentImageNode.location = (locatoinX, locationY)
             currentImageNode.name = 'p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
             # AlphaOver
-            if i > 0:
+            if i > partitionsStart:
                 currentAlphaOverNode = group.nodes.new(type = 'CompositorNodeAlphaOver')
                 locatoinX = i * 200 + 200
                 locationY = (i - 1) * -100
                 currentAlphaOverNode.location = (locatoinX, locationY)
                 currentAlphaOverNode.name = 'p_' + str(i) + '_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
                 # связи
-                if i == 1:
+                # if i == 1:
+                if i == (partitionsStart + 1):
                     group.links.new(group.nodes['p_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[1])
                 else:
                     group.links.new(group.nodes['p_' + str(i - 1) + '_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[1])
                 group.links.new(group.nodes['p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[2])
         # Output
         if partitionsCount == 1:
-            group.links.new(group.nodes['p_' + str(partitionsCount) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
+            group.links.new(group.nodes['p_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
         else:
-            group.links.new(group.nodes['p_' + str(partitionsCount - 1) + '_' + str(partitionsCount) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
+            group.links.new(group.nodes['p_' + str(partitionsEnd - 1) + '_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
         # добавить построенную связку в группу и в композитинг
         nodesField = bpy.context.scene.node_tree
         groupNode = nodesField.nodes.new(type = 'CompositorNodeGroup')
@@ -145,6 +170,32 @@ class PartRender(bpy.types.Operator):
         if returnAreaAfterCompositingRedraw not in bpy.app.handlers.scene_update_post:
             bpy.app.handlers.scene_update_post.append(returnAreaAfterCompositingRedraw)
         return {'FINISHED'}
+
+    def getInfoFileName(self):
+        if not self.__class__.dirPath:
+            if bpy.data.filepath:
+                self.__class__.dirPath = os.path.dirname(bpy.data.filepath) + os.path.sep + os.path.splitext(os.path.basename(bpy.data.filepath))[0]
+            else:
+                self.__class__.dirPath = os.path.dirname(bpy.context.user_preferences.filepaths.temporary_directory) + os.path.sep + 'partition_render_unsaved'
+            if not os.path.exists(self.__class__.dirPath):
+                os.makedirs(self.__class__.dirPath)
+        if not self.__class__.infoFile:
+            if bpy.data.filepath:
+                infoFileName = os.path.splitext(os.path.basename(bpy.data.filepath))[0]+'_pr.json'
+            else:
+                infoFileName = 'partition_render_unsaved_pr.json'
+            self.__class__.infoFile = self.__class__.dirPath + os.path.sep + infoFileName
+        return {'FINISHED'}
+
+    def checkFinish(self):
+        finish = False
+        if bpy.context.scene.partition_render_vars.useRange:
+            if self.__class__.currentPartitionNumber > bpy.context.scene.partition_render_vars.rangeTo:
+                finish = True
+        else:
+            if self.__class__.currentPartitionNumber > bpy.context.scene.partition_render_vars.xCut * bpy.context.scene.partition_render_vars.yCut:
+                finish = True
+        return finish
 
 def returnAreaAfterCompositingRedraw(scene):
     # Возврат окна в предыдущий режим, после показа композитинга для обновления
@@ -158,13 +209,11 @@ def onRenderPartitionFinished(scene):
     bpy.app.handlers.render_cancel.remove(onRenderPartitionCancel)
     # сохранить партицию
     partPath = PartRender.dirPath + os.path.sep + 'p_' + str(PartRender.currentPartitionNumber) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
-    oldFileFormat = bpy.context.scene.render.image_settings.file_format
-    oldColorMode = bpy.context.scene.render.image_settings.color_mode
+    PartRender.oldFileFormat = bpy.context.scene.render.image_settings.file_format
+    PartRender.oldColorMode = bpy.context.scene.render.image_settings.color_mode
     bpy.context.scene.render.image_settings.file_format = bpy.context.scene.partition_render_static.tmpFileFormat
     bpy.context.scene.render.image_settings.color_mode = bpy.context.scene.partition_render_static.tmpColorMode
     bpy.data.images['Render Result'].save_render(filepath = partPath)
-    bpy.context.scene.render.image_settings.file_format = oldFileFormat
-    bpy.context.scene.render.image_settings.color_mode = oldColorMode
     # установить номер следующей партиции
     setNextPartition()
     # запустить продолжение обработки
@@ -181,14 +230,18 @@ def onRenderPartitionCancel(scene):
 def renderPartition(scene):
     if renderPartition in bpy.app.handlers.scene_update_post:
         bpy.app.handlers.scene_update_post.remove(renderPartition)
+    # восстановить сохраненный формат файла
+    # здесь т.к. если в onRenderPartitionFinished сразу восстанавливать - иногда ошибка неверного контекста
+    bpy.context.scene.render.image_settings.file_format = PartRender.oldFileFormat
+    bpy.context.scene.render.image_settings.color_mode = PartRender.oldColorMode
     # запустить рендер очередной партиции
     return bpy.ops.render.partition_render()
 
 def setNextPartition():
     # установка номера следующей партиции
     PartRender.currentPartitionNumber += 1
-    with open(PartRender.infoFile, 'w') as infoFile:
-        json.dump({'currentPartition': PartRender.currentPartitionNumber}, infoFile, indent = 4)
+    with open(PartRender.infoFile, 'w') as currentFile:
+        json.dump({'currentPartition': PartRender.currentPartitionNumber}, currentFile, indent = 4)
     return {'FINISHED'}
 
 def register():
