d7f31248e5ba911d52d87f24709a88b4023ac678
Nikita Akimov <Korchy@users.noreply.github.com>
2017-02-07 19:11:40 +0300

    Update PartRender.py

--- PartRender.py
+++ PartRender.py
@@ -91,12 +91,32 @@ class PartRender(bpy.types.Operator):
         startRenderStatus = bpy.ops.render.render('INVOKE_DEFAULT')
         if startRenderStatus == {'CANCELLED'}:
             # Иногда по неопнятной причине возвращается {'CANCELLED'} и рендер не начинается
-            # В этом случае продолжить на следующем обновлении сцены
+            # В этом случае повторить на следующем обновлении сцены
             if renderPartition not in bpy.app.handlers.scene_update_post:
                 bpy.app.handlers.scene_update_post.append(renderPartition)
             return {'CANCELLED'}
         else:
             return {'FINISHED'}
+
+    @classmethod
+    def savePartitionToFile(cls):
+        # сохранить партицию во временный файл
+        if cls.dirPath:
+            partPath = cls.dirPath + os.path.sep + 'p_' + str(cls.currentPartitionNumber) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
+            cls.oldFileFormat = bpy.context.scene.render.image_settings.file_format
+            cls.oldColorMode = bpy.context.scene.render.image_settings.color_mode
+            bpy.context.scene.render.image_settings.file_format = bpy.context.scene.partition_render_static.tmpFileFormat
+            bpy.context.scene.render.image_settings.color_mode = bpy.context.scene.partition_render_static.tmpColorMode
+            # bpy.data.images['Render Result'].save_render(filepath = partPath)
+            for currentArea in bpy.context.window_manager.windows[0].screen.areas:
+                if currentArea.type == 'IMAGE_EDITOR':
+                    overrideArea = bpy.context.copy()
+                    overrideArea['area'] = currentArea
+                    bpy.ops.image.save_as(overrideArea, copy = True, filepath = partPath)
+                    break
+            bpy.context.scene.render.image_settings.file_format = cls.oldFileFormat
+            bpy.context.scene.render.image_settings.color_mode = cls.oldColorMode
+        return {'FINISHED'}
 
     @classmethod
     def reset(cls):
@@ -168,13 +188,16 @@ class PartRender(bpy.types.Operator):
                 # связи
                 # if i == 1:
                 if i == (partitionsStart + 1):
-                    group.links.new(group.nodes['p_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[1])
+                    imageOutputName = self.checkNodeOutputName(group.nodes['p_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension])
+                    group.links.new(group.nodes['p_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs[imageOutputName], currentAlphaOverNode.inputs[1])
                 else:
                     group.links.new(group.nodes['p_' + str(i - 1) + '_' + str(i) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[1])
-                group.links.new(group.nodes['p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], currentAlphaOverNode.inputs[2])
+                imageOutputName = self.checkNodeOutputName(group.nodes['p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension])
+                group.links.new(group.nodes['p_' + str(i + 1) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs[imageOutputName], currentAlphaOverNode.inputs[2])
         # Output
         if partitionsCount == 1:
-            group.links.new(group.nodes['p_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
+            imageOutputName = self.checkNodeOutputName(group.nodes['p_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension])
+            group.links.new(group.nodes['p_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs[imageOutputName], groupOutput.inputs['Out'])
         else:
             group.links.new(group.nodes['p_' + str(partitionsEnd - 1) + '_' + str(partitionsEnd) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension].outputs['Image'], groupOutput.inputs['Out'])
         # добавить построенную связку в группу и в композитинг
@@ -198,6 +221,16 @@ class PartRender(bpy.types.Operator):
             for currentNode in bpy.context.scene.node_tree.nodes:
                 if currentNode.name == 'partitionGroup' or currentNode.name == 'partitionComposite':
                     bpy.context.scene.node_tree.nodes.remove(currentNode)
+
+    def checkNodeOutputName(self, node):
+        outputName = 'Image'
+        for currentOutput in node.outputs:
+            if currentOutput.name == 'Image':
+                break
+            if currentOutput.name == 'Combined':
+                outputName = 'Combined'
+                break
+        return outputName
 
     @classmethod
     def getInfoFileName(cls):
@@ -234,27 +267,33 @@ def returnAreaAfterCompositingRedraw(scene):
 
 def onRenderPartitionFinished(scene):
     print('Partition '+str(PartRender.currentPartitionNumber) + ' finished')
-    bpy.app.handlers.render_complete.remove(onRenderPartitionFinished)
-    bpy.app.handlers.render_cancel.remove(onRenderPartitionCancel)
+    if onRenderPartitionFinished in bpy.app.handlers.render_complete:
+        bpy.app.handlers.render_complete.remove(onRenderPartitionFinished)
+    if onRenderPartitionCancel in bpy.app.handlers.render_cancel:
+        bpy.app.handlers.render_cancel.remove(onRenderPartitionCancel)
+    # запустить продолжение обработки
+    if afterRenderPartitionFinished not in bpy.app.handlers.scene_update_post:
+        bpy.app.handlers.scene_update_post.append(afterRenderPartitionFinished)
+    return {'FINISHED'}
+
+def afterRenderPartitionFinished(scene):
+    # После успешного завершения рендера очередной партиции
+    # на событии обновления сцены, иначе ошибки
+    if afterRenderPartitionFinished in bpy.app.handlers.scene_update_post:
+        bpy.app.handlers.scene_update_post.remove(afterRenderPartitionFinished)
     # сохранить партицию
-    if PartRender.dirPath:
-        partPath = PartRender.dirPath + os.path.sep + 'p_' + str(PartRender.currentPartitionNumber) + '.' + bpy.context.scene.partition_render_static.tmpFileExtension
-        PartRender.oldFileFormat = bpy.context.scene.render.image_settings.file_format
-        PartRender.oldColorMode = bpy.context.scene.render.image_settings.color_mode
-        bpy.context.scene.render.image_settings.file_format = bpy.context.scene.partition_render_static.tmpFileFormat
-        bpy.context.scene.render.image_settings.color_mode = bpy.context.scene.partition_render_static.tmpColorMode
-        bpy.data.images['Render Result'].save_render(filepath = partPath)
-    # установить номер следующей партиции
+    PartRender.savePartitionToFile()
+    # рендер следующей партиции
     PartRender.getInfoFileName()
     setNextPartition()
-    # запустить продолжение обработки
-    if renderPartition not in bpy.app.handlers.scene_update_post:
-        bpy.app.handlers.scene_update_post.append(renderPartition)
+    renderPartition(scene)
     return {'FINISHED'}
 
 def onRenderPartitionCancel(scene):
-    bpy.app.handlers.render_complete.remove(onRenderPartitionFinished)
-    bpy.app.handlers.render_cancel.remove(onRenderPartitionCancel)
+    if onRenderPartitionFinished in bpy.app.handlers.render_complete:
+        bpy.app.handlers.render_complete.remove(onRenderPartitionFinished)
+    if onRenderPartitionCancel in bpy.app.handlers.render_cancel:
+        bpy.app.handlers.render_cancel.remove(onRenderPartitionCancel)
     PartRender.executing = False
     # Перерисовать окно, иначе обновляет с задержкой
     for area in bpy.context.window_manager.windows[0].screen.areas:
@@ -266,12 +305,6 @@ def onRenderPartitionCancel(scene):
 def renderPartition(scene):
     if renderPartition in bpy.app.handlers.scene_update_post:
         bpy.app.handlers.scene_update_post.remove(renderPartition)
-    # восстановить сохраненный формат файла
-    # здесь т.к. если в onRenderPartitionFinished сразу восстанавливать - иногда ошибка неверного контекста
-    if PartRender.oldFileFormat:
-        bpy.context.scene.render.image_settings.file_format = PartRender.oldFileFormat
-    if PartRender.oldColorMode:
-        bpy.context.scene.render.image_settings.color_mode = PartRender.oldColorMode
     # запустить рендер очередной партиции
     return bpy.ops.render.partition_render()
 
